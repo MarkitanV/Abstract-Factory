25343af6d946d293a2820ada1f91691fec102f89
MarkitanV <valera01080054@gmail.com>
2022-08-17 23:47:25 +0300

    First commit

---
+++ Abstract Factory/Abstract Factory/Program.cs
@@ -0,0 +1,52 @@
+﻿//  Структура паттерна
+ 
+  abstract class AbstractFactory  //Абстрактный класс фабрики AbstractFactory определяет методы для создания объектов. Причем методы возвращают абстрактные продукты, а не их конкретные реализации.
+{
+    public abstract AbstractProductA CreateProductA();
+    public abstract AbstractProductB CreateProductB();
+}
+class ConcreteFactory1: AbstractFactory //Конкретные классы фабрик ConcreteFactory1 и ConcreteFactory2 реализуют абстрактные методы базового класса и непосредственно определяют какие конкретные продукты использовать
+{
+    public override AbstractProductA CreateProductA()
+    {
+        return new ProductA1();
+    }
+    public override AbstractProductB CreateProductB()
+    {
+        return new ProductB1();
+    }
+}
+class ConcreteFactory2 : AbstractFactory
+{
+    public override AbstractProductA CreateProductA()
+    {
+        return new ProductA2();
+    }
+
+    public override AbstractProductB CreateProductB()
+    {
+        return new ProductB2();
+    }
+}
+abstract class AbstractProductA { }  //Абстрактные классы AbstractProductA и AbstractProductB определяют интерфейс для классов, объекты которых будут создаваться в программе.
+abstract class AbstractProductB { }
+
+class ProductA1 : AbstractProductA { } //Конкретные классы ProductA1 / ProductA2 и ProductB1 / ProductB2 представляют конкретную реализацию абстрактных классов
+class ProductB1 : AbstractProductB { }
+
+class ProductA2: AbstractProductA { }
+
+class ProductB2: AbstractProductB { }
+
+class Client
+{
+    private AbstractProductA abstractProductA;
+    private AbstractProductB abstractProductB;
+
+    public Client(AbstractFactory factory)
+    {
+        abstractProductB = factory.CreateProductB();
+        abstractProductA = factory.CreateProductA();
+    }
+}
+// Класс клиента Client использует класс фабрики для создания объектов. При этом он использует исключительно абстрактный класс фабрики AbstractFactory и абстрактные классы продуктов AbstractProductA и AbstractProductB и никак не зависит от их конкретных реализаций
